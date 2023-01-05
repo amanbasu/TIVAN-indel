014fda50f9f62e077e4ed448c36d58bc66a488e6
Aman A <amanag.11@gmail.com>
2023-01-05 03:47:20 -0500

    make data creation generic

--- src/predict.py
+++ src/predict.py
@@ -1,38 +1,43 @@
 import numpy as np
-import json
 import xgboost as xgb
 from utils import *
 import argparse
+import pandas as pd
 
 np.random.seed(0)
-
-tissue = 'CD4_NAIVE'
 
 def predict_n_save(X, y, args):
     # load saved model
     model = xgb.Booster()
-    model.load_model(f'../res/{args.train_tissue}.json')
+    model.load_model(args.model)
 
     dtest = xgb.DMatrix(X, label=y)
     res = model.predict(dtest)
 
     stats = get_stats(y, res)
-    with open(f'../res/{args.test_tissue}_test.json', 'w', encoding='utf-8') as f:
-        json.dump(stats, f, ensure_ascii=False, indent=4)
+    print(stats)
+
+    ## save results
+    df = pd.DataFrame(res, columns=['score'])
+    df.to_csv(args.output, index=None, sep='\t')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Arguments for testing.'
     )
     parser.add_argument(
-        '--train_tissue', default='Whole_Blood', type=str, 
-        help='tissue for which the model is trained on'
+        '--path', default='../test/', type=str, 
+        help='folder containing data'
     )
     parser.add_argument(
-        '--test_tissue', default='CD4_NAIVE', type=str, 
-        help='tissue to be used for testing'
+        '--model', default='../res/model.json', type=str, 
+        help='saved model path'
+    )
+    parser.add_argument(
+        '--output', default='../res/output.txt', type=str, 
+        help='output path'
     )
     args = parser.parse_args()
 
-    X, y = read_features(args.test_tissue)
+    X, y = read_features(args.path)
     predict_n_save(X, y, args)
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 from sklearn.metrics import roc_auc_score, precision_recall_curve, auc
 import numpy as np
 import h5py
+import os
+import pandas as pd
 
 def get_stats(y, y_hat):
     # get area under the curve (auc) score
@@ -11,18 +13,20 @@ def get_stats(y, y_hat):
 
     return {'auroc': auroc, 'auprc': auprc}
 
-def norm(x):
-    return (x - np.min(x, axis=0)) / (np.max(x, axis=0) - np.min(x, axis=0))
+def read_features(path):
+    hf_fname = os.path.join(path, 'seq_features.h5')
+    hf = h5py.File(hf_fname, 'r')
 
-def read_features(tissue):
-    hf = h5py.File(f'../data/{tissue}.h5', 'r')
     ref = np.array(hf['feat_ref'])
     alt = np.array(hf['feat_alt'])
-    annot = np.array(hf['annot'])
     label = np.array(hf['label'])
     hf.close()
 
+    annot_fname = os.path.join(path, 'annot.csv')
+    annot = pd.read_csv(annot_fname)
+    annot.reset_index(drop=True, inplace=True)
+    annot.drop(columns=['RawScore', 'PHRED'], inplace=True)
+
     X = np.hstack([annot, ref, alt])
-    X = norm(X)
 
     return X, label
